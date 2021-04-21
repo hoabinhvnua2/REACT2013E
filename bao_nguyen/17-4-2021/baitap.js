1a77057aa3ba51ebe24a80288e3c863c132a6eca
thebao <anhbaonb24@gmail.com>
2021-04-21 08:57:12 -0500

    okk

--- bao_nguyen/17-4-2021/baitap.js
+++ bao_nguyen/17-4-2021/baitap.js
@@ -1,74 +1,103 @@
-var soNhapVao = +prompt('nhap so can nhap');
-// tinh so gia thua 
+var soNhapVao = +prompt("nhap so can nhap");
+// tinh so gia thua
 
-function giaThua(a){
-
+function giaThua(a) {
     let tong = 1;
-    for(let i = 1 ; i<=a ; i++){
-        tong = tong*a;
+    for (let i = 1; i <= a; i++) {
+        tong = tong * a;
     }
     return tong;
-    
 }
-console.log(giaThua(soNhapVao) + ' là giai thừa của ' + soNhapVao);
-console.log('--------------------------------------------------------')
-// so nhap vao co phai so hoan hao ko 
+console.log(giaThua(soNhapVao) + " là giai thừa của " + soNhapVao);
+console.log("--------------------------------------------------------");
+// so nhap vao co phai so hoan hao ko
 
-function soHoanHao(a){
+function soHoanHao(a) {
     let tong = 0;
-    for( let i=1 ; i <a ; i++){
-        if(a % i == 0){
-            tong = tong +i;
+    for (let i = 1; i < a; i++) {
+        if (a % i == 0) {
+            tong = tong + i;
         }
-    }  
-    if(tong ===  a){
+    }
+    if (tong === a) {
         return true;
-    }else{
+    } else {
         return false;
     }
-    
 }
-if(soHoanHao(soNhapVao)){
-    console.log( soNhapVao +'  là số hoàn hảo ')
-}else {
-    console.log( soNhapVao +'  ko phải là số hoàn hảo ')
+if (soHoanHao(soNhapVao)) {
+    console.log(soNhapVao + "  là số hoàn hảo ");
+} else {
+    console.log(soNhapVao + "  ko phải là số hoàn hảo ");
 }
-// so nguyen to 
-console.log('--------------------------------------------------------')
+// so nguyen to
+console.log("--------------------------------------------------------");
 
-function nguyenTo (a){
+function nguyenTo(a) {
     let kt = true;
-    if(a<=2){
-        kt = true ;
+    if (a <= 2) {
+        kt = true;
     }
-    for(let i=2 ; i<a ; i++){
-        if(a % i == 0){
+    for (let i = 2; i < a; i++) {
+        if (a % i == 0) {
             kt = false;
-            break
+            break;
         }
-
     }
     return kt;
 }
-if(nguyenTo(soNhapVao)){
-    console.log(soNhapVao+ ' là số nguyên tố ')
-}else{
-    console.log(soNhapVao+ ' không phải số nguyên tố ')
+if (nguyenTo(soNhapVao)) {
+    console.log(soNhapVao + " là số nguyên tố ");
+} else {
+    console.log(soNhapVao + " không phải số nguyên tố ");
 }
-console.log('--------------------------------------------------------')
-function soChinhPhuong(a){
-    let kt=false;
-    for( let i = 1; i < a ; i++){
-        if(i*i == a){
+console.log("--------------------------------------------------------");
+
+function soChinhPhuong(a) {
+    let kt = false;
+    for (let i = 1; i < a; i++) {
+        if (i * i == a) {
             kt = true;
-            break; 
+            break;
         }
     }
-    return kt 
+    return kt;
 }
-if(soChinhPhuong(soNhapVao)){
+if (soChinhPhuong(soNhapVao)) {
+    console.log(soNhapVao + " là só chính phương ");
+} else {
+    console.log(soNhapVao + " không phải là số chính phương ");
+}
+// yeeu caauf soos nhaapj đúng
+switch (soNhapVao > 0) {
+    case soNhapVao < 10:
+        console.log("số bạn nhập nhỏ hơn 10 ");
+        break;
+    case soNhapVao < 20:
+        console.log("số bạn nhập nhỏ hơn 20");
+        break;
+    case soNhapVao < 30:
+        console.log("số bạn nhập nhỏ hơn 30");
+        break;
+    case soNhapVao < 40:
+        console.log("số bạn nhập nhỏ hơn 40");
+        break;
+    case 41 < soNhapVao < 50:
+        console.log("số bạn nhập nhỏ hơn 50");
+        break;
+    case soNhapVao < 60:
+        console.log("số bạn nhập nhỏ hơn 60");
+        break;
+    case soNhapVao < 70:
+        console.log("số bạn nhập nhỏ hơn 70");
+        break;
+    case soNhapVao < 80:
+        console.log("số bạn nhập nhỏ hơn 80");
+        break;
+    case soNhapVao < 90:
+        console.log("số bạn nhập nhỏ hơn 90");
+        break;
+    default:
+        console.log("số bạn nhập nhỏ hơn 100");
 
-    console.log(soNhapVao+ ' là só chính phương ');
-}else{
-    console.log(soNhapVao+ ' không phải là số chính phương ');
 }
