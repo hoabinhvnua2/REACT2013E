0498e043ec1f0f127aee2603c3af4cf0fc39ef33
nguyentruonggiang <ntruonggiangtb98@gmail.com>
2021-05-29 15:03:43 +0700

    test

--- giang_nguyen/10-4-2021/vd.js
+++ giang_nguyen/10-4-2021/vd.js
@@ -1,19 +1,35 @@
-var height = prompt();
-var weight = prompt();
-var bmi1 = weight/(height*2);
-alert("nhập chiều cao của minh"+height);
-alert("nhập cân nặng của minh"+weight);
-alert(bmi1);
-var height = prompt();
-var weight = prompt();
-var bmi2 = weight/(height*2);
-alert("nhập chiều cao của toan"+height);
-alert("nhập cân nặng của toan"+weight);
-alert(bmi2);
-if(bmi1>bmi2){
-  alert("BMI của minh lớn hơn toàn");
-}else if(bmi1==bmi2){
-  alert("BMI của minh bằng toàn");
-}else{
-  alert("BMI của minh nhỏ hơn toàn");
-}
+// var height = prompt();
+// var weight = prompt();
+// var bmi1 = weight/(height*2);
+// alert("nhập chiều cao của minh"+height);
+// alert("nhập cân nặng của minh"+weight);
+// alert(bmi1);
+// var height = prompt();
+// var weight = prompt();
+// var bmi2 = weight/(height*2);
+// alert("nhập chiều cao của toan"+height);
+// alert("nhập cân nặng của toan"+weight);
+// alert(bmi2);
+// if(bmi1>bmi2){
+//   alert("BMI của minh lớn hơn toàn");
+// }else if(bmi1==bmi2){
+//   alert("BMI của minh bằng toàn");
+// }else{
+//   alert("BMI của minh nhỏ hơn toàn");
+// }
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=soccer",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "8e7149ce40mshf9a5aa260bf7178p1953dcjsn30b995fcfb3f",
+		"x-rapidapi-host": "livescore6.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
---
+++ giang_nguyen/22-5-2021/scss-study/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text1">tôi học lớp react</div>
+      <div className="text2">tôi học lớp react</div>
+      <div className="text3">tôi học lớp react</div>
+      <div className="text4">tôi học lớp react</div>
+      <div className="text5">tôi học lớp react</div>
+      <div className="text6">tôi học lớp react</div>
+    </div>
+  );
+}
+
+export default App;
