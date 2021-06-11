4920d241d2d4d2f2be12e10e9ab0edb32dcd64a3
thebao <anhbaonb24@gmail.com>
2021-06-11 02:16:51 -0500

    test

---
+++ bao_nguyen/9-6-2021/hook/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.scss';
+import './from/TestFrom'
+import TestFrom from './from/TestFrom';
+
+function App() {
+  return (
+    <div className="App">
+     <TestFrom className='from'>
+
+     </TestFrom>
+     
+    </div>
+  );
+}
+
+export default App;
