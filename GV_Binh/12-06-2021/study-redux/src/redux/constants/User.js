876194604b7585d81ddc594ae9bfc99320561098
= <tranhoabinh.tha.hua@gmail.com>
2021-07-01 21:29:44 +0700

    update day 22

--- GV_Binh/12-06-2021/study-redux/src/redux/constants/User.js
+++ GV_Binh/12-06-2021/study-redux/src/redux/constants/User.js
@@ -4,9 +4,21 @@ export const userAction = {
   GET_USER_FAI: "GET_USER_FAI",
 
   ADD_USER: "ADD_USER",
+  UPDATE_USER: "UPDATE_USER",
+  DELETE_USER: "DELETE_USER"
 };
 
 export const addNewUser = (param) => ({
   type: userAction.ADD_USER,
   payload: { user: param },
+});
+
+export const updateUser = (param) => ({
+  type: userAction.UPDATE_USER,
+  payload: param,
+});
+
+export const deleteUser = (param) => ({
+  type: userAction.DELETE_USER,
+  payload: param,
 });
--- GV_Binh/12-06-2021/study-redux/src/redux/reducers/User.js
+++ GV_Binh/12-06-2021/study-redux/src/redux/reducers/User.js
@@ -1,25 +1,36 @@
-import { userAction } from '../constants/User';
+import { userAction } from "../constants/User";
 
 const initialState = {
-    data: [{
-        user: 'React'
-    }],
-    err: '',
-    loading: false
+  data: [
+    {
+      user: "React",
+    },
+  ],
+  err: "",
+  loading: false,
+};
+
+
+if (localStorage.getItem("users")) {
+  initialState.data = JSON.parse(localStorage.getItem("users"));
 }
 
 const user = (state = initialState, action) => {
-    const { type, payload } = action
-    switch (type) {
-        case userAction.ADD_USER:
-            return {...state, data: [...state.data, payload]}
-        // case userAction.GET_USER_SUCCESS:
-        //     return {...state, loading: false, data: payload}
-        // case userAction.GET_USER_FAI:
-        //     return {...state, loading: false, err: payload}
-        default:
-            return state
-    }
-}
+  const { type, payload } = action;
+  switch (type) {
+    case userAction.ADD_USER:
+      return { ...state, data: [...state.data, payload] };
+    case userAction.UPDATE_USER:
+      return { ...state, data: payload };
+    case userAction.DELETE_USER:
+      return { ...state, data: payload };
+    // case userAction.GET_USER_SUCCESS:
+    //     return {...state, loading: false, data: payload}
+    // case userAction.GET_USER_FAI:
+    //     return {...state, loading: false, err: payload}
+    default:
+      return state;
+  }
+};
 
 export default user;
