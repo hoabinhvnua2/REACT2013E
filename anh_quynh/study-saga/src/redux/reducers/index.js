785c13d1036310b9cd515be37e63b40bcce614fe
quynhanh <quynhanhcr@gmail.com>
2021-07-10 18:39:30 +0700

    remove folder

--- anh_quynh/study-saga/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import products from './Product'
-
-const rootReducer = combineReducers({
-    products
-});
-
-export default rootReducer;
--- anh_quynh/study-saga/src/redux/sagas/index.js
+++
@@ -1,6 +0,0 @@
-import { fork} from 'redux-saga/effects';
-import product from './Product';
-
-export default function* rootSaga(){
-    yield fork(product)
-}
