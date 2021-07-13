5555115e32565789683b21eccabf834f409aa57b
thebao <anhbaonb24@gmail.com>
2021-07-13 03:53:45 -0500

    tesst

---
+++ bao_nguyen/bai-tap-cuo-khoa/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+import { BrowserRouter , Route ,Link ,Switch ,Redirect  } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+         
+        </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
--- bao_nguyen/bài-tập-cuối-khóa/my-app/src/redux/constands/count.js
+++
@@ -1,5 +0,0 @@
-export const countAction = {
-        cong:'cong',
-        tru:'tru'
-
-}
